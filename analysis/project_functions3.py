bf266fd0d9256aca67667d271a49a6895bd12c3b
YASH KUMAR <kyash03@student.ubc.ca>
2023-04-19 20:50:06 -0700

    done :)

--- analysis/project_functions3.py
+++ analysis/project_functions3.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+import math
+
 
 def load_and_process(path_to_csv_file):
     df_1 = pd.read_csv('../data/raw/data.csv').drop(
@@ -14,7 +16,10 @@ def load_and_process(path_to_csv_file):
         }).assign(diagnosis=lambda x: x['diagnosis'].map({
             'M': 1,
             'B': 0
-        })).assign(area_mean=lambda x: x['area_mean'].apply(lambda y: y if y > 0 else np.nan)).assign(perimeter_mean=lambda x: x['perimeter_mean'].apply(lambda y: y if y > 0 else np.nan)).sort_values(by='area_mean').reset_index(drop=True)
+        })).assign(area_mean=lambda x: x['area_mean'].apply(
+            lambda y: y if y > 0 else np.nan)).assign(
+                perimeter_mean=lambda x: x['perimeter_mean'].apply(
+                    lambda y: y if y > 0 else np.nan))
     # sorting by size of tumour
 
     return df_2
